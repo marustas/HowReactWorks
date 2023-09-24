03938d4a681f4c009a2228f0d5035aa2b9ab0120
Stanislau Marudau <marudov.stas@gmail.com>
2023-09-24 22:21:54 +0200

    Implemented key prop for correct diffing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 const content = [
   {
+    id: 0,
     summary: "React is a library for building UIs",
     details:
       "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
+    id: 1,
     summary: "State management is like giving state a home",
     details:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
+    id: 2,
     summary: "We can think of props as the component API",
     details:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -39,7 +42,8 @@ function Tabbed({ content }) {
       </div>
       {activeTab <= 2 ? (
         <TabContent
-          item={/*same as content[activeTab]*/ content.at(activeTab)}
+          key={content.at(activeTab).id}
+          tem={/*same as content[activeTab]*/ content.at(activeTab)}
         />
       ) : (
         <DifferentContent />
